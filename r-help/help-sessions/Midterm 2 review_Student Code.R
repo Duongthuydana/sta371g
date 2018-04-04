8b805cc1b4bd0b9b0a2eb31419bae91c3c857bc1
saiaravindhravi <35385650+saiaravindhravi@users.noreply.github.com>
2018-04-04 13:39:49 -0500

    Add files via upload

---
+++ r-help/help-sessions/Midterm 2 review_Student Code.R
@@ -0,0 +1,20 @@
+###
+### Sai - Houses dataset
+###
+
+houses_extended = read.csv('https://raw.githubusercontent.com/brianlukoff/sta371g/master/data/HousesExtended.csv')
+
+
+###
+### Han - state dataset
+###
+
+state <- read.csv("https://raw.githubusercontent.com/brianlukoff/sta371g/master/data/state.csv", header=T)
+
+
+
+###
+### Jared - cars dataset
+###
+
+cars = read.csv('https://faculty.mccombs.utexas.edu/carlos.carvalho/teaching/Cars.csv')
